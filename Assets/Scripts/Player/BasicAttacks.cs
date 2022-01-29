cc4db057143267a1f420e8e5d228c29b4b0596e8
Pixsea <jiin@uci.edu>
2022-01-29 03:37:45 -0800

    Added better player attack and movement scripts

--- Assets/Scripts/Player/BasicAttacks.cs
+++ Assets/Scripts/Player/BasicAttacks.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class BasicAttacks : MonoBehaviour
 {
-    private GameObject bm;  //Battle manager
-    private Phase battlePhase;
-
     // Animator for attacking
     [SerializeField]
     private GameObject pSprite;  // tell the animator script to attack
@@ -22,38 +19,17 @@ public class BasicAttacks : MonoBehaviour
     [SerializeField]
     private GameObject precision_projectile;
 
+    [HideInInspector]
+    public bool canAttack = true;
+    [HideInInspector]
+    public bool canRotate = true;  // Maybe use to control rotating in the direction while moving if attack cooldowns are too long
 
-    // Keys used for multi-directional shooting/Fire3
-    [SerializeField]
-    private string Attackkey;
-    [SerializeField]
-    private string Upkey;
-    [SerializeField]
-    private string Leftkey;
-    [SerializeField]
-    private string Downkey;
-    [SerializeField]
-    private string Rightkey;
     [SerializeField]
     private string Switchkey;
     [SerializeField]
     private string Guardkey;  // So you can't shoot while guarding
 
-    // Return true when held down
-    private bool Attackpress;
-    private bool Uppress;
-    private bool Leftpress;
-    private bool Downpress;
-    private bool Rightpress;
-    private bool Guardpress;
-
-    //return true on frame pushed down
-    private bool Switch;
-    private bool Switch2;  // Used so that it rememebers when a key is pressed down if an update frame happens without a fixed update
-
-    private Vector2 direction;
     private int mode;
-    private float cooldown;
 
     public List<GameObject> bullets = new List<GameObject>();  // List used to destroy all bullets
 
@@ -61,292 +37,105 @@ public class BasicAttacks : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        bm = GameObject.Find("BattleManager");
-
         mode = 0;
-        cooldown = 0f;
-        Switch2 = false;
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        Attackpress = Input.GetKey(Attackkey);
-        Uppress = Input.GetKey(Upkey);
-        Leftpress = Input.GetKey(Leftkey);
-        Downpress = Input.GetKey(Downkey);
-        Rightpress = Input.GetKey(Rightkey);
-
-        Switch = Input.GetKeyDown(Switchkey);
-
-        if (Switch)
+        if (Input.GetMouseButton(0) && canAttack)
         {
-            Switch2 = true;  // Remeber if true for fixed update, will get set back to false at first fixed update
+
+            StartCoroutine(ShootBullet());
         }
-
-        Guardpress = Input.GetKey(Guardkey);
-    }
-
-    void FixedUpdate()
-    {
-        battlePhase = bm.GetComponent<BattleManager>().currPhase;
-
-        // If not in combat, prevent attacking
-        if (battlePhase != Phase.Combat)
         {
-            Attackpress = false;
-            Uppress = false;
-            Leftpress = false;
-            Downpress = false;
-            Rightpress = false;
-            Switch2 = false;
-        }
 
-
-        if (Switch2)
-        {
-            Switch2 = false;
-
-            if (mode == 4)
-            {
-                mode = 0;
-            }
-
-            else
-            {
-                mode += 1;
-            }
         }
 
         
-        // Rotate to direction pressed
-        if (Uppress | Leftpress | Downpress | Rightpress)
+        if (Input.GetKeyDown(Switchkey))
         {
-            FreeRotate();
+            mode += 1;
+
+            if (mode > 4)
+            {
+                mode = 0;
+            }
         }
+    }
 
 
-        // Attacks in front of the player, should be used with Movement2
-        if (Attackpress && (cooldown <= 0) && !Guardpress)
+    private void FixedUpdate()
+    {
+        // Rotate towards mouse if trying to attack
+        if (Input.GetMouseButton(0))
         {
-            Attack(transform.rotation);
-            pSprite.SendMessage("Attack");
+            Vector3 object_pos = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+            Vector2 shootDirection = new Vector2(0, 0);
+            shootDirection.x = Input.mousePosition.x - object_pos.x;
+            shootDirection.y = Input.mousePosition.y - object_pos.y;
+            float angle = Mathf.Atan2(shootDirection.y, shootDirection.x) * Mathf.Rad2Deg;
+
+            Quaternion newDirection = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+            gameObject.transform.rotation = newDirection;
         }
+    }
 
 
-        // Attacks in direction pressed
-        //if ((Uppress | Leftpress | Downpress | Rightpress) && (cooldown <= 0))
-        //{
-        //    FreeAttack();
-        //    pSprite.SendMessage("Attack");
-        //}
+    IEnumerator ShootBullet()
+    {
+        canAttack = false;
+        Vector3 object_pos = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        Vector2 shootDirection = new Vector2(0, 0);
+        shootDirection.x = Input.mousePosition.x - object_pos.x;
+        shootDirection.y = Input.mousePosition.y - object_pos.y;
+        float angle = Mathf.Atan2(shootDirection.y, shootDirection.x) * Mathf.Rad2Deg;
 
-        cooldown--;
+        StartCoroutine(Attack(Quaternion.Euler(new Vector3(0, 0, angle - 90))));
+
+
+
+        yield return null;
     }
 
 
 
-    void Fire()
+    
+
+
+    IEnumerator Attack(Quaternion direction)
     {
-        Quaternion temp = Quaternion.Euler(0, 0, 0);
 
-        // Check direction
-        if (BasicMovement.playerDirection == 1)
-        {
-            temp = Quaternion.Euler(0, 0, 0);
-        }
-
-        else if (BasicMovement.playerDirection == 2)
-        {
-            temp = Quaternion.Euler(0, 0, 315);
-        }
-
-        else if (BasicMovement.playerDirection == 3)
-        {
-            temp = Quaternion.Euler(0, 0, 270);
-        }
-
-        else if (BasicMovement.playerDirection == 4)
-        {
-            temp = Quaternion.Euler(0, 0, 225);
-        }
-
-        else if (BasicMovement.playerDirection == 5)
-        {
-            temp = Quaternion.Euler(0, 0, 180);
-        }
-
-        else if (BasicMovement.playerDirection == 6)
-        {
-            temp = Quaternion.Euler(0, 0, 135);
-        }
-
-        else if (BasicMovement.playerDirection == 7)
-        {
-            temp = Quaternion.Euler(0, 0, 90);
-        }
-
-        else if (BasicMovement.playerDirection == 8)
-        {
-            temp = Quaternion.Euler(0, 0, 45);
-        }
-
-
-        // Making bullet spawn at the correct rotation, see first link in google doc
-        //Vector3 tempDirection = Quaternion.Euler(90, 0, 0) * temp;
-        //Quaternion tempDirection2 = Quaternion.LookRotation(Vector3.forward, tempDirection);
-        //GameObject bullet = Instantiate(projectile, transform.position, tempDirection2) as GameObject;
-
-        GameObject bullet = Instantiate(projectile, transform.position, temp) as GameObject;
-    }
-
-
-
-    void Attack(Quaternion direction)
-    {
         // Given an quaternion roation, attacks in that direction
         if (mode == 0)
         {
-            BasicShot(direction);
+            StartCoroutine(BasicShot(direction));
         }
 
         else if (mode == 1)
         {
-            RapidShot(direction);
+            StartCoroutine(RapidShot(direction));
         }
 
         else if (mode == 2)
         {
-            SpreadShot(direction);
+            StartCoroutine(SpreadShot(direction));
         }
 
         else if (mode == 3)
         {
-            SprayShot(direction);
+            StartCoroutine(SprayShot(direction));
         }
 
         else if (mode == 4)
         {
-            PrecisionShot(direction);
+            StartCoroutine(PrecisionShot(direction));
         }
+
+        yield return null;
     }
-
-
-
-    void FreeAttack()
-    {
-        // Attacks in the direction pressed, can attack in an independent direction from what you're moving
-
-        Quaternion temp = Quaternion.Euler(0, 0, 0);
-
-        if (Uppress && !Downpress && !Leftpress && !Rightpress)
-        {
-            temp = Quaternion.Euler(0, 0, 0);
-        }
-
-        else if (Uppress && !Downpress && !Leftpress && Rightpress)
-        {
-            temp = Quaternion.Euler(0, 0, 315);
-        }
-
-        else if (!Uppress && !Downpress && !Leftpress && Rightpress)
-        {
-            temp = Quaternion.Euler(0, 0, 270);
-        }
-
-        else if (!Uppress && Downpress && !Leftpress && Rightpress)
-        {
-            temp = Quaternion.Euler(0, 0, 225);
-        }
-
-        else if (!Uppress && Downpress && !Leftpress && !Rightpress)
-        {
-            temp = Quaternion.Euler(0, 0, 180);
-        }
-
-        else if (!Uppress && Downpress && Leftpress && !Rightpress)
-        {
-            temp = Quaternion.Euler(0, 0, 135);
-        }
-
-        else if (!Uppress && !Downpress && Leftpress && !Rightpress)
-        {
-            temp = Quaternion.Euler(0, 0, 90);
-        }
-
-        else if (Uppress && !Downpress && Leftpress && !Rightpress)
-        {
-            temp = Quaternion.Euler(0, 0, 45);
-        }
-
-        
-        Attack(temp);
-    }
-
-
-
-    void FreeRotate()
-    {
-        // Rotate to the direction pressed, can turn in an independent direction from what you're moving
-
-        Quaternion temp = Quaternion.Euler(0, 0, 0);
-
-        if (Uppress && !Downpress && !Leftpress && !Rightpress)
-        {
-            temp = Quaternion.Euler(0, 0, 0);
-        }
-
-        else if (Uppress && !Downpress && !Leftpress && Rightpress)
-        {
-            temp = Quaternion.Euler(0, 0, 315);
-        }
-
-        else if (!Uppress && !Downpress && !Leftpress && Rightpress)
-        {
-            temp = Quaternion.Euler(0, 0, 270);
-        }
-
-        else if (!Uppress && Downpress && !Leftpress && Rightpress)
-        {
-            temp = Quaternion.Euler(0, 0, 225);
-        }
-
-        else if (!Uppress && Downpress && !Leftpress && !Rightpress)
-        {
-            temp = Quaternion.Euler(0, 0, 180);
-        }
-
-        else if (!Uppress && Downpress && Leftpress && !Rightpress)
-        {
-            temp = Quaternion.Euler(0, 0, 135);
-        }
-
-        else if (!Uppress && !Downpress && Leftpress && !Rightpress)
-        {
-            temp = Quaternion.Euler(0, 0, 90);
-        }
-
-        else if (Uppress && !Downpress && Leftpress && !Rightpress)
-        {
-            temp = Quaternion.Euler(0, 0, 45);
-        }
-
-        transform.rotation = temp;
-    }
-
-
-
-    void Reset2()
-    {
-        mode = 0;
-        cooldown = 0f;
-
-        //foreach (GameObject bullet in bullets)
-        //{
-        //    Destroy(bullet);
-        //}
-    }
-
 
 
     ////////////////////
@@ -355,7 +144,7 @@ public class BasicAttacks : MonoBehaviour
 
 
 
-    void BasicShot(Quaternion direction)
+    IEnumerator BasicShot(Quaternion direction)
     {
         GameObject bullet1 = Instantiate(projectile, transform.position, direction) as GameObject;
         GameObject bullet2 = Instantiate(projectile, transform.position, direction * Quaternion.Euler(0, 0, 7)) as GameObject;
@@ -369,22 +158,32 @@ public class BasicAttacks : MonoBehaviour
         Physics2D.IgnoreCollision(bullet2.GetComponent<Collider2D>(), GetComponent<Collider2D>());
         Physics2D.IgnoreCollision(bullet3.GetComponent<Collider2D>(), GetComponent<Collider2D>());
 
-        cooldown = .6f / Time.fixedDeltaTime;
+
+        yield return new WaitForSeconds(.6f);
+
+        canAttack = true;
+
+        yield return null;
     }
 
 
 
-    void RapidShot(Quaternion direction)
+    IEnumerator RapidShot(Quaternion direction)
     {
         GameObject bullet = Instantiate(rapid_projectile, transform.position, direction) as GameObject;
         bullets.Add(bullet);
         Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), GetComponent<Collider2D>());
-        cooldown = .2f / Time.fixedDeltaTime;
+
+        yield return new WaitForSeconds(.2f);
+
+        canAttack = true;
+
+        yield return null;
     }
 
 
 
-    void SpreadShot(Quaternion direction)
+    IEnumerator SpreadShot(Quaternion direction)
     {
         GameObject bullet1 = Instantiate(spread_projectile, transform.position, direction) as GameObject;
         GameObject bullet2 = Instantiate(spread_projectile, transform.position, direction * Quaternion.Euler(0, 0, 10)) as GameObject;
@@ -410,27 +209,41 @@ public class BasicAttacks : MonoBehaviour
         Physics2D.IgnoreCollision(bullet6.GetComponent<Collider2D>(), GetComponent<Collider2D>());
         Physics2D.IgnoreCollision(bullet7.GetComponent<Collider2D>(), GetComponent<Collider2D>());
 
-        cooldown = 1.25f / Time.fixedDeltaTime;
+        yield return new WaitForSeconds(1.25f);
+
+        canAttack = true;
+
+        yield return null;
     }
 
 
 
-    void SprayShot(Quaternion direction)
+    IEnumerator SprayShot(Quaternion direction)
     {
         Quaternion temp = Quaternion.Euler(0, 0, Random.Range(-30.0f, 30.0f));
         GameObject bullet = Instantiate(spray_projectile, transform.position, direction * temp) as GameObject;
         bullets.Add(bullet);
         Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), GetComponent<Collider2D>());
-        cooldown = .1f / Time.fixedDeltaTime;
+
+        yield return new WaitForSeconds(.1f);
+
+        canAttack = true;
+
+        yield return null;
     }
 
 
 
-    void PrecisionShot(Quaternion direction)
+    IEnumerator PrecisionShot(Quaternion direction)
     {
         GameObject bullet = Instantiate(precision_projectile, transform.position, direction) as GameObject;
         bullets.Add(bullet);
         Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), GetComponent<Collider2D>());
-        cooldown = 2.5f / Time.fixedDeltaTime;
+
+        yield return new WaitForSeconds(2.5f);
+
+        canAttack = true;
+
+        yield return null;
     }
 }
--- Assets/Scripts/Player/BasicMovement.cs
+++ Assets/Scripts/Player/BasicMovement.cs
@@ -4,454 +4,63 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
-    private GameObject bm;  //Battle manager
-    private Phase battlePhase;
-
-    private float speed;  //Base speed of movement
+    [SerializeField]
+    private float speed = 10;
     private Vector2 velocity;  //Vector controlling movement
-    [HideInInspector]
-    public static int playerDirection;  //Int representing direction facing, 1 is up, 2 is up-right, ect.
-
-    //private bool isMoving;  //Makes it so you can't move in another direction while moving
-    private string last; //Remembers last move
 
 
     private Rigidbody2D rb;
+    private BasicAttacks ba;
 
-    // Movement keys
-    [SerializeField]
-    private string Upkey;
-    [SerializeField]
-    private string Leftkey;
-    [SerializeField]
-    private string Downkey;
-    [SerializeField]
-    private string Rightkey;
+    private float horizontalInput;
+    private float verticalInput;
 
-    // Return true when pushed down
-    private bool Uppress;
-    private bool Leftpress;
-    private bool Downpress;
-    private bool Rightpress;
 
-    // Return True on frame pushed down
-    private bool Updown;
-    private bool Leftdown;
-    private bool Downdown;
-    private bool Rightdown;
+    // For animations
+    [SerializeField]
+    //private Animator animator;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        bm = GameObject.Find("BattleManager");
-        speed = gameObject.GetComponent<PlayerStats>().speed;
-
         rb = GetComponent<Rigidbody2D>();
-        playerDirection = 1;
+        ba = GetComponent<BasicAttacks>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Uppress = Input.GetKey(Upkey);
-        Leftpress = Input.GetKey(Leftkey);
-        Downpress = Input.GetKey(Downkey);
-        Rightpress = Input.GetKey(Rightkey);
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
 
-        Updown = Input.GetKeyDown(Upkey);
-        Leftdown = Input.GetKeyDown(Leftkey);
-        Downdown = Input.GetKeyDown(Downkey);
-        Rightdown = Input.GetKeyDown(Rightkey);
+        //animator.SetFloat("Horizontal", rb.velocity.x);
+        //animator.SetFloat("Vertical", rb.velocity.y);
+        //animator.SetFloat("Speed", rb.velocity.sqrMagnitude);
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        battlePhase = bm.GetComponent<BattleManager>().currPhase;
-        speed = gameObject.GetComponent<PlayerStats>().speed;
-
-        // If not in combat or in the overworld, prevent movement
-        if ((battlePhase != Phase.Combat) && (battlePhase != Phase.Overworld))
-        {
-            Uppress = false;
-            Leftpress = false;
-            Downpress = false;
-            Rightpress = false;
-
-            Updown = false;
-            Leftdown = false;
-            Downdown = false;
-            Rightdown = false;
-        }
-
-
-        velocity = new Vector2(0.0f, 0.0f);
-
-        Movement3();
-
-        //velocity = new Vector2(0.0f, 0.0f);
-
-        // GroundMove();
-        //Movement2();
-        //CheckDirection();
-
-
-        // Limits max speed
-        //if (myRigid.velocity.magnitude > speed)
-        //{
-        //    myRigid.velocity = myRigid.velocity.normalized * speed;
-        //}
+        Movement();
     }
-
-
-
-    void MoveUp()
-    {
-        velocity.y += speed;
-        //transform.rotation = Quaternion.Euler(new Vector2(0.0f, 1.0f));
-        //direction = new Vector2(0.0f, 1.0f);
-        //isMoving = true;
-    }
-
-    void MoveDown()
-    {
-        velocity.y += -speed;
-        //transform.rotation = Quaternion.Euler(new Vector2(0.0f, -1.0f));
-        //direction = new Vector2(0.0f, -1.0f);
-        //isMoving = true;
-    }
-
-    void MoveLeft()
-    {
-        velocity.x += -speed;
-        //transform.rotation = Quaternion.Euler(new Vector2(-1.0f, 0.0f));
-        //direction = new Vector2(-1.0f, 0.0f);
-        //isMoving = true;
-    }
-
-    void MoveRight()
-    {
-        velocity.x += speed;
-        //transform.rotation = Quaternion.Euler(new Vector2(1.0f, 0.0f));
-        //direction = new Vector2(1.0f, 0.0f);
-        //isMoving = true;
-    }
-
-
-
-    //void GroundMove()
-    //{
-    //    //Basically makes it so if you press a direction while holding another, the new direction is registered
-    //    if (isMoving)
-    //    {
-    //        // Check for new direction
-    //        if (Updown)
-    //        {
-    //            last = "Up";
-    //        }
-    //        else if (Leftdown)
-    //        {
-    //            last = "Left";
-    //        }
-    //        else if (Downdown)
-    //        {
-    //            last = "Down";
-    //        }
-    //        else if (Rightdown)
-    //        {
-    //            last = "Right";
-    //        }
-
-
-    //        // Keep current direction
-    //        if ((Uppress) & (last == "Up"))
-    //        {
-    //            MoveUp();
-    //        }
-
-    //        else if ((Leftpress) & (last == "Left"))
-    //        {
-    //            MoveLeft();
-    //        }
-
-    //        else if ((Downpress) & (last == "Down"))
-    //        {
-    //            MoveDown();
-    //        }
-
-    //        else if ((Rightpress) & (last == "Right"))
-    //        {
-    //            MoveRight();
-    //        }
-
-    //        else
-    //        {
-    //            isMoving = false;
-    //            last = "";
-    //        }
-
-    //    }
-
-
-    //    //Else check for a new direction
-    //    else if (Uppress)
-    //    {
-    //        MoveUp();
-    //        // rigidbody.AddTorque( x, y, z) continuously rotates
-    //    }
-
-    //    else if (Leftpress)
-    //    {
-    //        MoveLeft();
-    //    }
-
-    //    else if (Downpress)
-    //    {
-    //        MoveDown();
-    //    }
-
-    //    else if (Rightpress)
-    //    {
-    //        MoveRight();
-    //    }
-
-    //    else
-    //    {
-    //        isMoving = false;
-    //        last = "";
-    //    }
-    //}
-
-
 
     void Movement()
     {
-        // If holding two opposite directions, cancel both
-        if (Uppress && !Downpress)
+        velocity = new Vector2(horizontalInput * speed, verticalInput * speed);
+        rb.velocity = velocity;
+
+
+
+        if (horizontalInput != 0 || verticalInput != 0)
         {
-            MoveUp();
-        }
-
-        if (Leftpress && !Rightpress)
-        {
-            MoveLeft();
-        }
-
-        if (Downpress && !Uppress)
-        {
-            MoveDown();
-        }
-
-        if (Rightpress && !Leftpress)
-        {
-            MoveRight();
-        }
-
-
-        // If going diagonal, limit speed
-        if ((playerDirection == 2) | (playerDirection == 4) | (playerDirection == 6) | (playerDirection == 8))
-        {
-            velocity.x = velocity.x * 2 / 3;
-            velocity.y = velocity.y * 2 / 3;
-        }
-
-        // Actually move the player
-        transform.Translate(velocity * Time.deltaTime);
-    }
-
-
-
-    void Movement2()
-    {
-        //Check direction, then rotate to that direction and apply velocity that way
-
-        if (Uppress && !Downpress && !Leftpress && !Rightpress)
-        {
-            playerDirection = 1;
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-            //velocity.y += speed;
-            velocity = new Vector2(0.0f, speed);
-        }
-
-        else if (Uppress && !Downpress && !Leftpress && Rightpress)
-        {
-            playerDirection = 2;
-            transform.rotation = Quaternion.Euler(0, 0, 315);
-            //velocity.y += speed;
-            velocity = new Vector2(speed * .7071f, speed * .7071f);
-        }
-
-        else if (!Uppress && !Downpress && !Leftpress && Rightpress)
-        {
-            playerDirection = 3;
-            transform.rotation = Quaternion.Euler(0, 0, 270);
-            //velocity.y += speed;
-            velocity = new Vector2(speed, 0.0f);
-        }
-
-        else if (!Uppress && Downpress && !Leftpress && Rightpress)
-        {
-            playerDirection = 4;
-            transform.rotation = Quaternion.Euler(0, 0, 225);
-            //velocity.y += speed;
-            velocity = new Vector2(speed * .7071f, -speed * .7071f);
-        }
-
-        else if (!Uppress && Downpress && !Leftpress && !Rightpress)
-        {
-            playerDirection = 5;
-            transform.rotation = Quaternion.Euler(0, 0, 180);
-            //velocity.y += speed;
-            velocity = new Vector2(0.0f,-speed);
-        }
-
-        else if (!Uppress && Downpress && Leftpress && !Rightpress)
-        {
-            playerDirection = 6;
-            transform.rotation = Quaternion.Euler(0, 0, 135);
-            //velocity.y += speed;
-            velocity = new Vector2(-speed * .7071f, -speed * .7071f);
-        }
-
-        else if (!Uppress && !Downpress && Leftpress && !Rightpress)
-        {
-            playerDirection = 7;
-            transform.rotation = Quaternion.Euler(0, 0, 90);
-            //velocity.y += speed;
-            velocity = new Vector2(-speed, 0.0f);
-        }
-
-        else if (Uppress && !Downpress && Leftpress && !Rightpress)
-        {
-            playerDirection = 8;
-            transform.rotation = Quaternion.Euler(0, 0, 45);
-            //velocity.y += speed;
-            velocity = new Vector2(-speed * .7071f, speed * .7071f);
-        }
-
-        //transform.Translate(velocity * Time.deltaTime);
-        //transform.Translate(velocity);
-        rb.velocity = velocity * Time.fixedDeltaTime;
-    }
-
-
-
-    void Movement3()
-    {
-        //Check direction, then apply velocity in that direction
-
-        if (Uppress && !Downpress && !Leftpress && !Rightpress)
-        {
-            playerDirection = 1;
-            //transform.rotation = Quaternion.Euler(0, 0, 0);
-            //velocity.y += speed;
-            velocity = new Vector2(0.0f, speed);
-        }
-
-        else if (Uppress && !Downpress && !Leftpress && Rightpress)
-        {
-            playerDirection = 2;
-            //transform.rotation = Quaternion.Euler(0, 0, 315);
-            //velocity.y += speed;
-            velocity = new Vector2(speed * .7071f, speed * .7071f);
-        }
-
-        else if (!Uppress && !Downpress && !Leftpress && Rightpress)
-        {
-            playerDirection = 3;
-            //transform.rotation = Quaternion.Euler(0, 0, 270);
-            //velocity.y += speed;
-            velocity = new Vector2(speed, 0.0f);
-        }
-
-        else if (!Uppress && Downpress && !Leftpress && Rightpress)
-        {
-            playerDirection = 4;
-            //transform.rotation = Quaternion.Euler(0, 0, 225);
-            //velocity.y += speed;
-            velocity = new Vector2(speed * .7071f, -speed * .7071f);
-        }
-
-        else if (!Uppress && Downpress && !Leftpress && !Rightpress)
-        {
-            playerDirection = 5;
-            //transform.rotation = Quaternion.Euler(0, 0, 180);
-            //velocity.y += speed;
-            velocity = new Vector2(0.0f, -speed);
-        }
-
-        else if (!Uppress && Downpress && Leftpress && !Rightpress)
-        {
-            playerDirection = 6;
-            //transform.rotation = Quaternion.Euler(0, 0, 135);
-            //velocity.y += speed;
-            velocity = new Vector2(-speed * .7071f, -speed * .7071f);
-        }
-
-        else if (!Uppress && !Downpress && Leftpress && !Rightpress)
-        {
-            playerDirection = 7;
-            //transform.rotation = Quaternion.Euler(0, 0, 90);
-            //velocity.y += speed;
-            velocity = new Vector2(-speed, 0.0f);
-        }
-
-        else if (Uppress && !Downpress && Leftpress && !Rightpress)
-        {
-            playerDirection = 8;
-            //transform.rotation = Quaternion.Euler(0, 0, 45);
-            //velocity.y += speed;
-            velocity = new Vector2(-speed * .7071f, speed * .7071f);
-        }
-
-        //transform.Translate(velocity * Time.deltaTime);
-        //transform.Translate(velocity);
-        rb.velocity = velocity * Time.fixedDeltaTime;
-    }
-
-
-
-    void CheckDirection()
-    {
-        if (Uppress && !Downpress && !Leftpress && !Rightpress)
-        {
-            playerDirection = 1;
-        }
-
-        else if (Uppress && !Downpress && !Leftpress && Rightpress)
-        {
-            playerDirection = 2;
-        }
-
-        else if (!Uppress && !Downpress && !Leftpress && Rightpress)
-        {
-            playerDirection = 3;
-        }
-
-        else if (!Uppress && Downpress && !Leftpress && Rightpress)
-        {
-            playerDirection = 4;
-        }
-
-        else if (!Uppress && Downpress && !Leftpress && !Rightpress)
-        {
-            playerDirection = 5;
-        }
-
-        else if (!Uppress && Downpress && Leftpress && !Rightpress)
-        {
-            playerDirection = 6;
-        }
-
-        else if (!Uppress && !Downpress && Leftpress && !Rightpress)
-        {
-            playerDirection = 7;
-        }
-
-        else if (Uppress && !Downpress && Leftpress && !Rightpress)
-        {
-            playerDirection = 8;
+            // If not in attack cooldown, rotate towards the direction moving
+            if (ba.canAttack)
+            {
+                float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+                Quaternion newDirection = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+                gameObject.transform.rotation = Quaternion.RotateTowards(gameObject.transform.rotation, newDirection, 30);
+            }
         }
     }
 }
